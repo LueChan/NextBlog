223fd44293b0ce66868e6a52dc5447bd555b270a
LueChan <LueChan@yeah.net>
2024-04-04 16:43:53 +0800

    Update index.js

--- themes/starter/index.js
+++ themes/starter/index.js
@@ -229,42 +229,6 @@ const Layout404 = (props) => {
 /**
  * 分类列表
  * @param {*} props
-            * @returns
-            */
-const LayoutCategoryIndex = (props) => {
-  const { locale } = useGlobal()
-  const { categoryOptions } = props
-  return (
-    <>
-      <div className='bg-white dark:bg-gray-700 px-10 py-10 shadow'>
-        <div className='dark:text-gray-200 mb-5'>
-          <i className='mr-4 fas fa-th' />{locale.COMMON.CATEGORY}:
-        </div>
-        <div id='category-list' className='duration-200 flex flex-wrap'>
-          {categoryOptions?.map(category => {
-            return (
-              <Link
-                key={category.name}
-                href={`/category/${category.name}`}
-                passHref
-                legacyBehavior>
-                <div
-                  className={'hover:text-black dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600 px-5 cursor-pointer py-2 hover:bg-gray-100'}>
-                  <i className='mr-4 fas fa-folder' />{category.name}({category.count})
-                </div>
-              </Link>
-            )
-          })}
-        </div>
-      </div>
-    </>
-  )
-}
-
-
-/**
- * 分类列表
- * @param {*} props
  * @returns
  */
 const LayoutCategoryIndex = props => {
