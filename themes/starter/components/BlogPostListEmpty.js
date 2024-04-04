1f5e4a6d8613844fe81b43eb9459669522d95544
LueChan <LueChan@yeah.net>
2024-04-04 22:56:48 +0800

    Update BlogPostListEmpty.js

--- themes/starter/components/BlogPostListEmpty.js
+++ themes/starter/components/BlogPostListEmpty.js
@@ -8,10 +8,12 @@ import { useGlobal } from '@/lib/global'
 const BlogPostListEmpty = ({ currentSearch }) => {
   const { locale } = useGlobal()
   return (
+    <div className='grow mt-20'>
     <div className="container mx-auto">  
       <div className='flex w-full items-center justify-center min-h-screen mx-auto md:-mt-20'>
         <div className='text-gray-500 dark:text-gray-300'>{locale.COMMON.NO_MORE} {(currentSearch && <div>{currentSearch}</div>)}</div>
       </div>
+    </div>
     </div>
   )
 }
